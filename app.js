329b4df41333e67e2d9ad5ef9eb76245b38f070b
Reiquem <dpls.diogo@outlook.com>
2023-10-06 15:06:14 -0300

    Working with enums

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
+// const person: {
+// 	name: string
+// 	age: number
+// 	hobbies: string[]
+// 	role: [number, string]
+// } = {
+// 	name: 'Diogo',
+// 	age: 26,
+// 	hobbies: ['Sports', 'Cooking', 'Gaming', 'Coding', 'Reading', 'Writing'],
+// 	role: [2, 'Developer']
+// }
+// const person = {
+// 	name: 'Diogo',
+// 	age: 26,
+// 	hobbies: ['Sports', 'Cooking', 'Gaming', 'Coding', 'Reading', 'Writing'],
+// 	role: [2, 'Developer']
+// }
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'Diogo',
     age: 26,
     hobbies: ['Sports', 'Cooking', 'Gaming', 'Coding', 'Reading', 'Writing'],
-    role: [2, 'Developer']
+    role: Role.ADMIN
 };
-person.role.push('admin');
-person.role.push[1] = 10;
-person.role = [0, 'admin', 'user'];
+// person.role.push('admin')
+// person.role.push[1] = 10
+// person.role = [0, 'admin', 'user']
 var favoriteActivities;
 favoriteActivities = ['Coding'];
 console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) // !!! ERROR !!!
+}
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }
--- app.ts
+++ app.ts
@@ -1,13 +1,37 @@
-const person: {
-	name: string
-	age: number
-	hobbies: string[]
-	role: [number, string]
-} = {
+// const person: {
+// 	name: string
+// 	age: number
+// 	hobbies: string[]
+// 	role: [number, string]
+// } = {
+// 	name: 'Diogo',
+// 	age: 26,
+// 	hobbies: ['Sports', 'Cooking', 'Gaming', 'Coding', 'Reading', 'Writing'],
+// 	role: [2, 'Developer']
+// }
+
+// const person = {
+// 	name: 'Diogo',
+// 	age: 26,
+// 	hobbies: ['Sports', 'Cooking', 'Gaming', 'Coding', 'Reading', 'Writing'],
+// 	role: [2, 'Developer']
+// }
+
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+
+enum Role {
+	ADMIN = 'ADMIN',
+	READ_ONLY = 100,
+	AUTHOR = 'AUTHOR'
+}
+
+const person = {
 	name: 'Diogo',
 	age: 26,
 	hobbies: ['Sports', 'Cooking', 'Gaming', 'Coding', 'Reading', 'Writing'],
-	role: [2, 'Developer']
+	role: Role.ADMIN
 }
 
 // person.role.push('admin')
@@ -22,4 +46,9 @@ console.log(person)
 
 for (const hobby of person.hobbies) {
 	console.log(hobby.toUpperCase())
+	// console.log(hobby.map()) // !!! ERROR !!!
+}
+
+if (person.role === Role.ADMIN) {
+	console.log('is admin')
 }
