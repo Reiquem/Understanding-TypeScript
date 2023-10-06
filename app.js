b240421e4d05dab8233749eed40978c4d92828d7
Reiquem <dpls.diogo@outlook.com>
2023-10-06 13:43:05 -0300

    arrays types

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var person = {
     name: 'Diogo',
-    age: '26'
+    age: 26,
+    hobbies: ['Sports', 'Cooking', 'Gaming', 'Coding', 'Reading', 'Writing']
 };
-console.log(person.name);
+var favoriteActivities;
+favoriteActivities = ['Coding'];
+console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
 const person: {
 	name: string
 	age: number
+	hobbies: string[]
 } = {
 	name: 'Diogo',
-	age: 26
+	age: 26,
+	hobbies: ['Sports', 'Cooking', 'Gaming', 'Coding', 'Reading', 'Writing']
 }
 
-console.log(person.name)
+let favoriteActivities: string[]
+favoriteActivities = ['Coding']
+
+console.log(person)
+
+for (const hobby of person.hobbies) {
+	console.log(hobby.toUpperCase())
+}
